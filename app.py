b0d00681a5a96f224d44e0c8b47b41911dcb78ca
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-05-10 14:59:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import requests
 import plotly.graph_objs as go
 
 
-st.set_page_config(page_title="NBA Shot Visualizer", page_icon='https://juststickers.in/wp-content/uploads/2015/05/basket-ball-player-1-decal.png', initial_sidebar_state="expanded")
+st.set_page_config(page_title="NBA Shot Visualizer", page_icon='https://juststickers.in/wp-content/uploads/2015/05/basket-ball-player-1-decal.png', initial_sidebar_state="expanded",layout='wide')
 
 currentyear = datetime.datetime.now().year
 
