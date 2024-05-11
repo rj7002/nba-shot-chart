5f4c485764aafa2e8c66f8f7357fe91ec3885f88
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-05-11 13:51:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import requests
 import plotly.graph_objs as go
 from abc import ABC, abstractmethod
 import plotly.express as px
-import mplcursors
 
 
 st.set_page_config(page_title="NBA Shot Visualizer", page_icon='https://juststickers.in/wp-content/uploads/2015/05/basket-ball-player-1-decal.png', initial_sidebar_state="expanded")
