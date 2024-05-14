916de1b6e1d541d6351c6384ec92bf88d31db92d
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-05-13 17:00:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -171,6 +171,74 @@ def draw_court(ax=None, color='black', lw=2, outer_lines=False):
     ax.set_facecolor('#D2B48C')
     return ax
 
+def draw_court2(ax=None, color='black', lw=2, outer_lines=False):
+    # If an axes object isn't provided to plot onto, just get current one
+    if ax is None:
+        ax = plt.gca()
+
+    # Create the various parts of an NBA basketball court
+
+    # Create the basketball hoop
+    # Diameter of a hoop is 18" so it has a radius of 9", which is a value
+    # 7.5 in our coordinate system
+    hoop = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
+
+    # Create backboard
+    backboard = Rectangle((-30, -7.5), 60, -1, linewidth=lw, color=color)
+
+    # The paint
+    # Create the outer box 0f the paint, width=16ft, height=19ft
+    outer_box = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color=color,
+                          fill=False)
+    # Create the inner box of the paint, widt=12ft, height=19ft
+    inner_box = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color=color,
+                          fill=False)
+
+    # Create free throw top arc
+    top_free_throw = Arc((0, 142.5), 120, 120, theta1=0, theta2=180,
+                         linewidth=lw, color=color, fill=False)
+    # Create free throw bottom arc
+    bottom_free_throw = Arc((0, 142.5), 120, 120, theta1=180, theta2=0,
+                            linewidth=lw, color=color, linestyle='dashed')
+    # Restricted Zone, it is an arc with 4ft radius from center of the hoop
+    restricted = Arc((0, 0), 80, 80, theta1=0, theta2=180, linewidth=lw,
+                     color=color)
+
+    # Three point line
+    # Create the side 3pt lines, they are 14ft long before they begin to arc
+    corner_three_a = Rectangle((-220, -47.5), 0, 140, linewidth=lw,
+                               color=color)
+    corner_three_b = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
+    # 3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
+    # I just played around with the theta values until they lined up with the 
+    # threes
+    three_arc = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw,
+                    color=color)
+
+    # Center Court
+    center_outer_arc = Arc((0, 422.5), 120, 120, theta1=180, theta2=0,
+                           linewidth=lw, color=color)
+    center_inner_arc = Arc((0, 422.5), 40, 40, theta1=180, theta2=0,
+                           linewidth=lw, color=color)
+
+    # List of the court elements to be plotted onto the axes
+    court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
+                      bottom_free_throw, restricted, corner_three_a,
+                      corner_three_b, three_arc, center_outer_arc,
+                      center_inner_arc]
+
+    if outer_lines:
+        # Draw the half court line, baseline and side out bound lines
+        outer_lines = Rectangle((-250, -47.5), 500, 470, linewidth=lw,
+                                color=color, fill=False)
+        court_elements.append(outer_lines)
+
+    # Add the court elements onto the axes
+    for element in court_elements:
+        ax.add_patch(element)
+    ax.set_facecolor('white')
+    return ax
+
 def create_court(ax, color):
     # Short corner 3PT lines
     ax.set_facecolor('#D2B48C')
@@ -1316,14 +1384,25 @@ shot_data["SECONDS_REMAINING"].astype(str)
         # Show plot
 # Display the image in Streamlit
                     # Customize color bar legend
-        plottype = st.selectbox('Plot Type',['Make/Miss','Hexbin Plot','Heat Map'])
+        plottype = st.selectbox('Plot Type',['Make/Miss','Hexbin Plot','Heat Map','KDE Plot'])
         if plottype == 'Make/Miss':
             st.plotly_chart(fig,use_container_width=True)
         elif plottype == 'Hexbin Plot':
             fig2.patch.set_visible(False)
             st.pyplot(fig2)
-        else:
+        elif plottype =='Heat Map':
             st.plotly_chart(fig5,use_container_width=True)
+        else:
+            plt.clf()
+            cmap = 'gist_heat_r'
+            fig6 = sns.kdeplot(x=-(shot_data['LOC_X']), y=shot_data['LOC_Y'], cmap=cmap, fill=True, n_levels=50)
+            draw_court2(fig6,outer_lines=True)
+            # Set the background color to white
+            fig6.set_facecolor('white')
+            fig6.set_xlim(250, -250)
+            fig6.set_ylim(-47.5, 422.5)
+            fig6.set_axis_off()
+            st.pyplot(fig6.figure)
         st.markdown(f'<div style="text-align: center;"><span style="font-size:25px;">{SEASON}: {total_makes}/{total_shots} - {shootperc}%</span></div>', unsafe_allow_html=True)
                 
 
